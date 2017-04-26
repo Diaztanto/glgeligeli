546eccabcbaca74689f37e3c1cf63d2d9a8e8971
Diaztanto <diaztanto.h@gmail.com>
2017-04-26 14:21:47 +0700

    minor changes

--- hape.c
+++ hape.c
@@ -1,3 +1,10 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace glm;
+
 #include <GL/glut.h>
 #include <math.h>
 #include <stdlib.h>
